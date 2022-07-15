e5292fe768ee944209db61f99ed29fdc2a942ee9
sebaborjas <108552013+sebaborjas@users.noreply.github.com>
2022-07-14 21:37:21 -0300

    llamado api y mostrar carta

--- calculadora-propinas-json-server/js/app.js
+++ calculadora-propinas-json-server/js/app.js
@@ -6,6 +6,12 @@ let cliente = {
   platillo: [],
 };
 
+const categoriaMenuPlatos = {
+  1: "Comida",
+  2: "Bebida",
+  3: "Postre",
+};
+
 crearOrdenBtn.addEventListener("click", crearOrden);
 
 function crearOrden(e) {
@@ -19,6 +25,7 @@ function crearOrden(e) {
   }
   guardarCliente(mesa, hora);
   mostrarSecciones();
+  imprimirCarta();
 }
 
 function mostrarMensaje(texto) {
@@ -44,4 +51,61 @@ function guardarCliente(mesa, hora) {
 function mostrarSecciones() {
   const seccionesOcultas = document.querySelectorAll(".d-none");
   seccionesOcultas.forEach((seccion) => seccion.classList.remove("d-none"));
+}
+
+function imprimirCarta() {
+  const url = "http://localhost:4000/platillos/";
+  fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((resultado) => {
+      imprimirHTML(resultado);
+    });
+}
+
+function imprimirHTML(menu) {
+  const contenidoPlato = document.querySelector("#platillos .contenido");
+  menu.forEach((plato) => {
+    const { nombre, precio, categoria, id } = plato;
+
+    //Creo fila para almacenar datos utilizo grid de bootstrap
+    const row = document.createElement("div");
+    row.classList.add("row", "py-3", "border-top");
+
+    //Div para almacenar el nombre del plato
+    const nombrePlato = document.createElement("div");
+    nombrePlato.classList.add("col-md-4");
+    nombrePlato.textContent = nombre;
+
+    //Div para el precio
+    const precioPlato = document.createElement("div");
+    precioPlato.classList.add("col-md-3", "fw-bold");
+    precioPlato.textContent = `$${precio}`;
+
+    //Div para la categoria
+    const categoriaPlato = document.createElement("div");
+    categoriaPlato.classList.add("col-md-3");
+    categoriaPlato.textContent = categoriaMenuPlatos[categoria];
+
+    //Input para saber cuantos platos agrego a la orden
+    const inputCantidad = document.createElement("input");
+    inputCantidad.type = "number";
+    inputCantidad.min = 0;
+    inputCantidad.value = 0;
+    inputCantidad.id = id;
+    inputCantidad.classList.add("form-control");
+    console.log(inputCantidad);
+
+    //Div para el input
+    const inputDiv = document.createElement("div");
+    inputDiv.classList.add("col-md-2");
+    //Agrego el input al div
+    inputDiv.appendChild(inputCantidad);
+
+    //Los agrego como columnas de mi fila
+    row.appendChild(nombrePlato);
+    row.appendChild(precioPlato);
+    row.appendChild(categoriaPlato);
+    row.appendChild(inputDiv);
+    contenidoPlato.appendChild(row);
+  });
 }
